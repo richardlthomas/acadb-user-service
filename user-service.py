94ceb9e873c2c53e4728a3495c623b8105b39560
richard.thomas <richard.thomas@mercurygate.com>
2017-08-10 13:37:30 -0400

    Added routing back in that went missing somehow. Still getting connection issues

--- user-service.py
+++ user-service.py
@@ -19,7 +19,7 @@ app.config['MONGO_HOST'] = db_user+':'+db_user_pass+'@aea-userdb-shard-00-00-diq
                                                     'shard-00-01-diqae.mongodb.net:27017,aea-userdb-shard-00-02-diqae' \
                                                     '.mongodb.net:27017/aea-userdb?ssl=true&replicaSet=aea-userdb-' \
                                                     'shard-0&authSource=admin'
-
+@app.route('/')
 def home():
     users = mongo.db.users.find()
     return render_template('index.html', users=users)
